fdb521be52d2641f796aeedae3c1fa60f7a1164f
Yuske Kuoka <yusuke.kuoka@surrealdb.com>
2025-11-21 13:53:08 +0000

    Implement DropColumn

--- internal/connector/server/migrator/drop_column.go
+++ internal/connector/server/migrator/drop_column.go
@@ -2,6 +2,9 @@ package migrator
 
 import (
 	"context"
+	"fmt"
+
+	surrealdb "github.com/surrealdb/surrealdb.go"
 )
 
 // DropColumn removes a column from non-history-mode tables.
@@ -11,10 +14,24 @@ import (
 // According to the Fivetran Partner SDK documentation, this operation should:
 // - Execute: ALTER TABLE <schema.table> DROP COLUMN <column_name>
 func (m *Migrator) DropColumn(ctx context.Context, schema, table, column string) error {
-	// TODO: Implement drop column logic
-	// 1. Remove the field from the table:
-	//    REMOVE FIELD column ON table
-	// 2. Return any errors encountered
+	// 1. Remove the field definition from the table schema
+	removeQuery := fmt.Sprintf("REMOVE FIELD %s ON %s", column, table)
+	_, err := surrealdb.Query[any](ctx, m.db, removeQuery, nil)
+	if err != nil {
+		return fmt.Errorf("failed to drop column %s from table %s: %w", column, table, err)
+	}
+
+	// 2. Remove the field values from all existing records
+	updateQuery := fmt.Sprintf("UPDATE %s UNSET %s", table, column)
+	_, err = surrealdb.Query[any](ctx, m.db, updateQuery, nil)
+	if err != nil {
+		return fmt.Errorf("failed to remove values for column %s in table %s: %w", column, table, err)
+	}
+
+	m.LogInfo("Dropped column",
+		"table", table,
+		"column", column,
+	)
 
 	return nil
 }
