6c4b8f914f2cc9a98d808f164a78e4d92b75b3cb
Yuske Kuoka <yusuke.kuoka@surrealdb.com>
2025-11-21 14:25:24 +0000

    Implement DropTable

--- internal/connector/server/migrator/drop_table.go
+++ internal/connector/server/migrator/drop_table.go
@@ -2,6 +2,9 @@ package migrator
 
 import (
 	"context"
+	"fmt"
+
+	surrealdb "github.com/surrealdb/surrealdb.go"
 )
 
 // DropTable removes a table from the database.
@@ -11,10 +14,13 @@ import (
 // According to the Fivetran Partner SDK documentation, this operation should:
 // - Execute: DROP TABLE <schema.table>
 func (m *Migrator) DropTable(ctx context.Context, schema, table string) error {
-	// TODO: Implement drop table logic
-	// 1. Remove the table from the database:
-	//    REMOVE TABLE table
-	// 2. Return any errors encountered
+	removeQuery := fmt.Sprintf("REMOVE TABLE %s", table)
+	_, err := surrealdb.Query[any](ctx, m.db, removeQuery, nil)
+	if err != nil {
+		return fmt.Errorf("failed to drop table %s: %w", table, err)
+	}
+
+	m.LogInfo("Dropped table", "table", table)
 
 	return nil
 }
